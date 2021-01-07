9e7fb5a55a3f5f98dd4daab919186b4e6ebc8551
wesley <wesley0983@yahoo.com.tw>
2021-01-08 00:54:02 +0800

    調整；引用標籤時，需與檔案名稱一致 新增；header init

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,18 @@
 <template>
   <header>
     <div class="header">
-      <h1>21</h1>
+      <img src="https://assets.website-files.com/5ee8e4acb0f1113df06e0942/5eff23cc3eab87cb7a7e6a15_Altar%20Logo%402x.png" alt="">
+      <div class="header_menus">
+        <div class="menus-item">Features</div>
+        <div class="menus-item">Pricing</div>
+        <div class="menus-item">Blog</div>
+        <div class="menus-item">About Us</div>
+        <div class="menus-item">Support</div>
+      </div>
+      <div class="loginbar">
+        <div class="login-btn">Log In</div>
+        <div class="login-btn">Get Started</div>
+      </div>
     </div>
   </header>
 </template>
@@ -9,5 +20,5 @@
 
 </script>
 <style>
-
+@import "../assets/header.scss";
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header></header>
+    <Header></Header>
     <Nuxt />
   </div>
 </template>
